b63e6c8b3924f06551e9c3afb00d8d542e72bf6b
Firmansyah Nuralif Rohman <mendoanjoe@gmail.com>
2024-01-16 09:33:42 +0700

    change to static vision

--- components/section/vision.tsx
+++ components/section/vision.tsx
@@ -1,57 +1,7 @@
-import { useEffect, useRef, useState } from "react";
-
 export default function Vision() {
-  const visionRef = useRef(null);
-  const visionContainerRef = useRef(null);
-  const [isVision, setIsVision] = useState(false);
-
-  useEffect(() => {
-    const visionHandle = () => {
-      const section = visionRef.current;
-      const sectionRect = section.getBoundingClientRect();
-      const windowHeight = window.innerHeight;
-
-      // Calculate the position of the section relative to the viewport
-      const sectionTop = sectionRect.top;
-      const sectionBottom = sectionRect.bottom;
-
-      const progress = (sectionTop / (sectionRect.height - windowHeight)) * -100
-
-      console.log(sectionTop * -1)
-      console.log(">")
-      console.log(((sectionRect.height - windowHeight) - ((windowHeight - visionContainerRef.current.getBoundingClientRect().height) / 2)))
-      console.log(sectionTop * -1 > ((sectionRect.height - windowHeight) - ((windowHeight - visionContainerRef.current.getBoundingClientRect().height) / 2)))
-      // console.log((sectionRect.height - windowHeight) - ((windowHeight - visionContainerRef.current.getBoundingClientRect().height)))
-
-      if (progress > 0 && progress <= 100) {
-        if (sectionTop * -1 < ((windowHeight - visionContainerRef.current.getBoundingClientRect().height) / 2)) {
-          visionContainerRef.current.style.transform= `translateY(${(sectionTop * -2)}px)`
-        } else if (sectionTop * -1 > ((sectionRect.height - windowHeight) - ((windowHeight - visionContainerRef.current.getBoundingClientRect().height)))) {
-          visionContainerRef.current.style.transform= `translateY(${(sectionTop * -1) + ((windowHeight - visionContainerRef.current.getBoundingClientRect().height))}px)`;
-        }
-        else {
-          visionContainerRef.current.style.transform= `translateY(${(sectionTop * -1) + ((windowHeight - visionContainerRef.current.getBoundingClientRect().height) / 2)}px)`;
-        }
-      }
-
-      // console.log((windowHeight - visionContainerRef.current.getBoundingClientRect().height))
-
-      // if (progress > 200 || progress < 0) {
-      //   visionContainerRef.current.style = null
-      // }
-    };
-
-    window.addEventListener("scroll", visionHandle);
-
-    return () => {
-      window.removeEventListener("scroll", visionHandle);
-    };
-  }, [visionRef, visionContainerRef]);
-
   return (
-    <div ref={visionRef} className="bg-[#E6EDFF] h-[300svh]">
-      <div className="h-[100svh] relative">
-        <div ref={visionContainerRef} className=" max-w-[1440px] mx-auto p-[max(48px,_min(calc(100vw_*_(80_/_1440)),_80px))_max(20px,_min(calc(100vw_*_(70_/_1440)),_70px))] overflow-hidden bg-[url('/image/vision-background.png')] bg-contain bg-no-repeat bg-right">
+    <div className="bg-[#E6EDFF]">
+      <div className=" max-w-[1440px] mx-auto p-[max(48px,_min(calc(100vw_*_(80_/_1440)),_80px))_max(20px,_min(calc(100vw_*_(70_/_1440)),_70px))] overflow-hidden bg-[url('/image/vision-background.png')] bg-contain bg-no-repeat bg-right">
           <div className="grid grid-cols-2 gap-[max(32px,_min(calc(100vw_*_(60_/_1440)),_60px))] max-sm:flex max-sm:flex-col">
             <div className="max-sm:hidden">
               <img
@@ -90,7 +40,6 @@ export default function Vision() {
             </div>
           </div>
         </div>
-      </div>
     </div>
   );
 }
