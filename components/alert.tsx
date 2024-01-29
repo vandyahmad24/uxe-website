1c35e20b225c51a8eb9c455d46161943b891b52d
Anas AL-Zghoul <anas.aref@almuhark.ae>
2024-01-29 08:01:09 +0400

    run azure pipeline again

--- components/alert.tsx
+++ components/alert.tsx
@@ -9,6 +9,7 @@ export default function Alert({ preview }) {
       className={cn('border-b', {
         'bg-accent-7 border-accent-7 text-white': preview,
         'bg-accent-1 border-accent-2': !preview,
+        
       })}
     >
       <Container>
