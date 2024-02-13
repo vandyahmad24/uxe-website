74e02164000ade34b23622de0b7ff8ff9df2144f
Anas AL-Zghoul <anas.aref@almuhark.ae>
2024-02-13 07:56:19 +0400

    add error page

--- pages/_error.js
+++ pages/_error.js
@@ -10,6 +10,7 @@ function Error({ statusCode }) {
    
   Error.getInitialProps = ({ res, err }) => {
     console.log(err);
+    console.log(res);
     const statusCode = res ? res.statusCode : err ? err.statusCode : 404
     return { statusCode }
   }
